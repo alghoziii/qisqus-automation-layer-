13f6622a9e1b05a481bc8518b904b4a6786902bd
alghozii_zyy <shobarudin67@students.amikom.ac.id>
2025-06-24 00:13:31 +0700

    update code

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"qiscus-agent-allocator/config"
 	"qiscus-agent-allocator/controller"
 
@@ -8,7 +9,9 @@ import (
 )
 
 func main() {
-	config.InitDB()
+	if err := config.InitDB(); err != nil {
+		log.Fatal("Failed to connect to DB:", err)
+	}
 
 	router := gin.Default()
 	router.POST("/webhook/agent_allocation", controller.WebhookHandler(config.DB))
--- config/db.go
+++ config/db.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"qiscus-agent-allocator/model"
 
@@ -13,23 +12,27 @@ import (
 
 var DB *gorm.DB
 
-func InitDB() {
+func InitDB() error {
 	_ = godotenv.Load()
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"))
+		os.Getenv("DB_PORT"),
+	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Database connection failed:", err)
+		return err
+	}
+
+	if err := db.AutoMigrate(&model.Customer{}, &model.Queue{}); err != nil {
+		return err
 	}
 
 	DB = db
 	fmt.Println("Connected to database")
-
-	db.AutoMigrate(&model.Customer{}, &model.Queue{})
+	return nil
 }
